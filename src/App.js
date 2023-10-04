27c595580fce257f37cef1f911edc56610d9c104
Furious <glamias@gmail.com>
2023-10-03 23:16:41 -0700

    deploy change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-// import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AnimateRoutes from "./components/AnimateRoutes";
 import Modal from "./components/Modal";
 import { useState } from "react";
